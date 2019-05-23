3d5ee08c26d9d0e9c764fcba64084c1339d72c56
Lokesh Kumar Balu <lokbalu@publicisgroupe.net>
2019-05-23 23:36:25 +0530

    Update comments

--- Git-DeleteMergedBranches.sh
+++ Git-DeleteMergedBranches.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Switch to master branch before deleting merged branches.
 git checkout master
 git pull
 
+# Get all the merged branches with master.
 mergedBranches=$(git branch --merged master | grep feature*)
 
+# Loop through the merged branches and delete them.
 for branch in $mergedBranches
 do 
     git push --delete origin $branch
