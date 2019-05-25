0090b3d00b924cef90298ea330072bec239cda31
Lokesh Balu <lokeshkumarbalu@gmail.com>
2019-05-25 11:13:38 +0530

    Check if working directory is clean

--- Git-PullBranchUpdates.sh
+++ Git-PullBranchUpdates.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Get all the local branches.
-localBranches=$(git branch | sed -e 's/*/ /g')
-
-# Loop through the local branches and update them one by one.
-for branch in $localBranches
-do
-    git checkout $branch
-    git pull
-done
-
+# Check if the working tree is clean
+currentStatus=$(git status)
+if [[ $currentStatus != *"working directory clean"* ]]; 
+then
+    # Print message if uncommited changes found.
+    echo "Working directory is not clean, commit or discard changes."
+else
+    # Get all the local branches.
+    localBranches=$(git branch | sed -e 's/*/ /g')
+    
+    # Loop through the local branches and update them one by one.
+    for branch in $localBranches
+    do
+        git checkout $branch
+	git pull
+    done
+fi
 
