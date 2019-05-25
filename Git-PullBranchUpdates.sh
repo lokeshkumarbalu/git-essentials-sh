5991a3f8fe80fdc32017e9f4e6738de998307994
Lokesh Balu <lokeshkumarbalu@gmail.com>
2019-05-25 19:43:04 +0530

    Update information message

--- Git-PullBranchUpdates.sh
+++ Git-PullBranchUpdates.sh
@@ -6,7 +6,7 @@ differenceCount=$(git diff-index --name-only HEAD | wc -w)
 if [[ $differenceCount -gt 0 ]];
 then
     # Print message if uncommited changes found.
-    echo "Working directory is not clean, commit or discard changes."
+    echo "Working tree is not clean, commit or discard changes."
 else
     # Get all the local branches.
     localBranches=$(git branch | sed -e 's/*/ /g')
