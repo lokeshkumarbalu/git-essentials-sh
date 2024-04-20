0233a7a751ea79d8ccd88b531eaf4529f97fc007
Lokesh Balu <lokeshkumarbalu@gmail.com>
2024-04-20 16:12:43 +0200

    Setup build and deploy

--- build/Program.cs
+++ build/Program.cs
@@ -1,7 +1,26 @@
 ﻿using build;
-using Bullseye.Internal;
+using Bullseye;
 
-const string OutputFolder = "output";
+const string outputFolder = "output";
+const string gitInstallationPath = @"C:\Program Files\Git\usr\bin";
 
+Targets.Target(Operation.Clean, Targets.ForEach(outputFolder), Utilities.EnsureDirectoryDeleted);
+Targets.Target(Operation.CreateFolders, Targets.ForEach(outputFolder), Utilities.EnsureDirectories);
+Targets.Target(Operation.InitialiseFolders, Targets.DependsOn(Operation.Clean, Operation.CreateFolders));
 
+Targets.Target(Operation.CopyShellScripts, () => Utilities.CopyShellScriptsToGit("src", gitInstallationPath));
+Targets.Target(Operation.Default, Targets.DependsOn(Operation.InitialiseFolders));
+await Targets.RunTargetsAndExitAsync(args);
 
+internal static class Operation
+{
+    public const string Clean = "clean";
+    
+    public const string CreateFolders = "create-folders";
+    
+    public const string InitialiseFolders = "initialise-folders";
+    
+    public const string Default = "default";
+    
+    public const string CopyShellScripts = "copy-shell-scripts";
+}
--- build/Utilities.cs
+++ build/Utilities.cs
@@ -35,4 +35,29 @@ public static class Utilities
 
     public static void DotNetToolRestore() =>
         Command.Run("dotnet", "tool restore");
+    
+    public static void CopyShellScriptsToGit(string source, string destination)
+    {
+        var sourceDir = new DirectoryInfo(source);
+        var destinationDir = new DirectoryInfo(destination);
+        if (!sourceDir.Exists)
+        {
+            Console.WriteLine($"Source directory {source} does not exist");
+            return;
+        }
+
+        if (!destinationDir.Exists)
+        {
+            Console.WriteLine($"Destination directory {destination} does not exist");
+            return;
+        }
+
+        var files = sourceDir.GetFiles("*.sh");
+        foreach (var file in files)
+        {
+            var destFile = Path.Combine(destination, file.Name);
+            Console.WriteLine($"Copying {file.FullName} to {destFile}");
+            file.CopyTo(destFile, true);
+        }
+    }
 }
